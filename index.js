ae3d13883acc09aa98305b037dee08ed1fce1436
Renekris <renekrispohlak@gmail.com>
2022-09-21 09:29:02 +0300

    Add ability to display elements

--- index.js
+++ index.js
@@ -12,13 +12,8 @@ const createPlayer = (playerName) => {
 }
 
 const gameBoard = (() => {
+    let board = ['X', 'X', 'O', 'O', 'X', 'O', 'X', 'O', 'X'];
     let _players = [];
-
-    //DOM Cache
-
-
-    //Bind Events
-
 
     function addPlayer(playerName) {
         if (_players.length <= 2) {
@@ -26,16 +21,56 @@ const gameBoard = (() => {
         }
     }
 
+    function createBoard() {
+        while (board.length <= 9) {
+            board.push(null);
+        }
+    }
+
+    function resetBoard() {
+        board = [];
+        createBoard();
+    }
+
+    function changeBoardValue(index, value) {
+        board[board.indexOf(index)] = value;
+    }
 
 
     return {
         addPlayer,
+        createBoard,
+        resetBoard,
+        changeBoardValue,
+        board,
         _players, //testing
     }
 })();
 
 const displayController = (() => {
 
+    //DOM Cache
+    const elGameBoard = document.getElementById('game-board');
+
+    //Bind Events
+
+
+    function displayBoard() {
+        gameBoard.board.forEach(element => {
+            const p = document.createElement('p');
+            p.addEventListener('pointerdown', updateValue)
+            p.textContent = element;
+            elGameBoard.append(p);
+        })
+    }
+
+    function updateValue(e) {
+        console.log(e.target);
+    }
+
+    return {
+        displayBoard,
+    }
 })();
 
 const scoreBoard = (() => {
@@ -44,5 +79,7 @@ const scoreBoard = (() => {
 
 
 //temp
-gameBoard.addPlayer('Renekris');
-gameBoard._players[0].getPlayerName;
+// gameBoard.addPlayer('Renekris');
+// gameBoard._players[0].getPlayerName;
+
+displayController.displayBoard();
