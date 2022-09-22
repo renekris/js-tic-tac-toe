4c478d7c19f3b0b02a71589d46ea0c72ad8a5f67
Renekris <renekrispohlak@gmail.com>
2022-09-23 02:43:48 +0300

    Add clicking board elements updates cell /w cross

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const createPlayer = (playerName, playerBoardPiece) => {
+const Player = (playerName, playerBoardPiece) => {
     let playerScore = null;
     const getPlayerName = () => playerName;
 
@@ -20,54 +20,44 @@ const createPlayer = (playerName, playerBoardPiece) => {
 
 const gameBoard = (() => {
     let currentTurn = null;
-    // let board = ['X', 'X', 'O', 'O', 'X', 'O', 'X', 'O', 'X'];
     let board = [];
     let _players = [];
 
     function _addPlayer(playerName, playerBoardPiece) {
         if (_players.length >= 2) return;
-        _players.push(createPlayer(playerName, playerBoardPiece.toUpperCase()));
+        _players.push(Player(playerName, playerBoardPiece.toUpperCase()));
     }
 
     function createPlayers(firstPlayer, secondPlayer = 'AI') {
         _addPlayer(firstPlayer, 'cross');
         _addPlayer(secondPlayer, 'circle');
-        currentTurn = _players[0]; //cross goes first
+        currentTurn = _players[0].getPlayerName(); //cross goes first
 
         console.log(`Player 1: ${_players[0].getPlayerName()}`);
         console.log(`Player 2: ${_players[1].getPlayerName()}`);
-        console.log(currentTurn.getPlayerName());
     }
 
-    function deletePlayers() {
+    function resetBoardAll() {
+        currentTurn = null;
+        board = [];
         _players = [];
-    }
 
-    function createBoard() {
         while (board.length < 9) {
             board.push(null);
         }
     }
 
-    function resetBoard() {
-        board = [];
-        createBoard();
-    }
-
     function changeBoardValue(index, value) {
-        board[board.indexOf(index)] = value;
+        board[index] = value;
     }
 
 
     return {
         createPlayers,
-        deletePlayers,
-        createBoard,
-        resetBoard,
+        resetBoardAll,
         changeBoardValue,
-        currentTurn,
-        board,
-        _players, //testing
+        getCurrentTurn: () => { return currentTurn },
+        getBoard: () => { return board },
     }
 })();
 
@@ -77,16 +67,17 @@ const displayController = (() => {
     const elGameBoard = document.getElementById('game-board');
     const elMenuForm = document.getElementById('menu-form');
     const elCpuToggle = document.getElementById('with-ai');
+    const elCurrentTurn = document.getElementById('current-turn');
 
     //Bind Events
     elMenuForm.addEventListener('submit', _menuFormSubmit);
     elCpuToggle.addEventListener('change', _checkToggle);
 
     //Init
-    function init() {
-        gameBoard.createBoard();
-        displayBoard();
-    }
+    // function init() {
+    //     gameBoard.createBoard();
+    //     displayBoard();
+    // }
 
     //Private
     function _checkToggle(e) {
@@ -94,67 +85,71 @@ const displayController = (() => {
     }
 
     function _displayTurn() {
-
+        elCurrentTurn.innerHTML = '';
+        const p = document.createElement('p');
+        p.textContent = `${gameBoard.getCurrentTurn()}'s turn`;
+        elCurrentTurn.append(p);
     }
 
-    function _updateValue(e) {
-        console.log(e.target.parentElement.dataset.index);
-        const targetValue = e.target.parentElement.className;
+    function _clickUpdateValue(e) {
         const targetIndex = e.target.parentElement.dataset.index;
-        gameBoard.changeBoardValue(targetIndex, targetValue);
+        gameBoard.changeBoardValue(targetIndex, 'cross');
+
+        displayBoard();
     }
 
     function _menuFormSubmit(e) {
-
-        gameBoard.deletePlayers();
-        if (e.target[0].value !== '' && e.target[1].value === '' && e.target[2].checked) { //with AI
+        if (e.target[0].value !== '' && e.target[1].value === '' && e.target[2].checked) {
+            //with AI
+            resetDisplayBoardAll(); //has to be inside so it wouldn't trigger during else condition
             gameBoard.createPlayers(e.target[0].value);
-        } else if (e.target[0].value !== '' && e.target[1].value !== '' && !e.target[2].checked) { //without AI
+        } else if (e.target[0].value !== '' && e.target[1].value !== '' && !e.target[2].checked) {
+            //without AI / PvP
+            resetDisplayBoardAll();
             gameBoard.createPlayers(e.target[0].value, e.target[1].value);
         } else {
             console.log('%cPlease enter required data', 'color:red')
             return
         }
-        console.dir(e.target);
-        e.target.classList.add('hidden');
 
 
+        e.target.classList.add('hidden'); //turn to modal
+        _displayTurn();
     }
 
-    //Public
-    function displayBoard() {
+    function _drawBoard() {
         let index = 0;
-        gameBoard.board.forEach(value => {
+        gameBoard.getBoard().forEach(value => {
             const elMainDiv = document.createElement('div');
             const elImageDiv = document.createElement('div');
-            elImageDiv.addEventListener('pointerup', _updateValue)
-            // if (value === 'X') {
-            // elMainDiv.classList.add('cross');
-            // } else if (value === 'O') {
-            //     elMainDiv.classList.add('circle');
-            // }
+            elImageDiv.addEventListener('pointerup', _clickUpdateValue)
+            if (value === 'cross') {
+                elMainDiv.classList.add('cross');
+            } else if (value === 'circle') {
+                elMainDiv.classList.add('circle');
+            }
             elMainDiv.dataset.index = index++;
             elMainDiv.append(elImageDiv)
             elGameBoard.append(elMainDiv);
         })
     }
 
-
-    function displayClearBoard() {
+    //Public
+    function displayBoard() {
         elGameBoard.innerHTML = '';
-        gameBoard.resetBoard();
+        _drawBoard();
+    }
+
+
+    function resetDisplayBoardAll() {
+        gameBoard.resetBoardAll();
+        displayBoard();
     }
 
     return {
-        init,
-        displayBoard,
-        displayClearBoard,
     }
 })();
 
 const scoreBoard = (() => {
 
 })();
-
-
-displayController.init();
